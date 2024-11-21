6ef92727072c0b6ce767905af25d8ba2161f61d9
amandaagaldino <amandagaldinox@gmail.com>
2024-11-21 08:32:58 -0300

    js trocando a cor do texto

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const input = document.getElementById('numero');
+        const span = document.getElementById('colorido');
+
+        input.addEventListener('input', () => {
+            const valor = parseInt(input.value);
+
+            if (valor <= 0) {
+                span.style.color = 'black';
+                alert('Número inválido');
+            } else if (valor >=1 && valor <=2 ) {
+                span.style.color = '#720000';
+            }else if (valor >=3) {
+                    span.style.color = '#64B67C';
+            } else {
+                span.style.color = 'black';
+            }
+        });
+        
